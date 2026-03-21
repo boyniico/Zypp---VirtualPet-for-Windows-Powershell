c20e7749a00b0dae235de0fd8898b1dfbb1e0167
Nico <nicolas.andres.bh@gmail.com>
2026-03-20 22:26:04 -0300

    Two esential functions were added

--- service/checks.py
+++ service/checks.py
@@ -1,9 +1,10 @@
 from service import json_manage as jm
+from datetime import date, datetime
 def check_and_reset() -> bool:
     time = jm.load_json("game/timecheck.json")
     if not time["game_start"]:
         DEFAULT_TIME = {
-            "game_start": None,
+            "game_start": datetime.now().isoformat(),
             "newborn": None,
             "last_visit": None,
             "last_food": None,
@@ -30,3 +31,13 @@ def check_and_reset() -> bool:
         
         return True
     return False
+def days_since_game_started() -> int:
+    time_data = jm.load_json("game/timecheck.json")
+    if not time_data.get("game_start"):
+        return 0
+    start = datetime.fromisoformat(time_data["game_start"])
+    now = datetime.now()
+    days_count = (now - start).days
+    time_data["days"] = days_count
+    jm.save_json("game/timecheck.json", time_data)
+    return days_count
