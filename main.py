bffadd1f297ac4576d60dc71fa59aea7e3f53cc2
Nico <nicolas.andres.bh@gmail.com>
2026-03-20 22:24:32 -0300

    Main now shows the main menu

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from datetime import datetime, date
 from service import checks
 
 checks.check_and_reset()
+days = checks.days_since_game_started()
 
 time = json_manage.load_json("game/timecheck.json")
 zypp = json_manage.load_json("game/pet.json")
 food = json_manage.load_json("game/food.json")
 player = json_manage.load_json("game/player.json")
-zyppsprites = json_manage.load_json("game/zyppsprite.json")
+
+zyppsprite = sprites.select_sprite()
+cls()
+interaction.show_zypp_menu(zypp,zyppsprite, days)
